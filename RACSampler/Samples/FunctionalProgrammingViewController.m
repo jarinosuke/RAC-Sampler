db154fd61ab07a42bf339eeeab389f70cf2d188a
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2014-01-16 01:09:57 +0900

    Implemented functional programming(map, filter, fold).

--- RACSampler/Samples/FunctionalProgrammingViewController.m
+++ RACSampler/Samples/FunctionalProgrammingViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "FunctionalProgrammingViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface FunctionalProgrammingViewController ()
-
+@property (nonatomic) IBOutlet UILabel *sequenceLabel;
+@property (nonatomic) IBOutlet UILabel *answerLabel;
+@property (nonatomic) RACSequence *sequence;
 @end
 
 @implementation FunctionalProgrammingViewController
@@ -18,7 +21,8 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        //You don't need to understand RACSequence.
+        self.sequence = @[@(1), @(2), @(3), @(4), @(5)].rac_sequence;
     }
     return self;
 }
@@ -26,13 +30,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.sequenceLabel.text = [self buildTextFromNumberArray:[self.sequence array]];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Button Action
+- (IBAction)mapButtonTapped:(id)sender
+{
+    //1. Map
+    NSArray *mappedArray = [[self.sequence map:^id(id value) {
+        return @(pow([value integerValue], 2));
+    }] array];
+    self.answerLabel.text = [self buildTextFromNumberArray:mappedArray];
+}
+
+- (IBAction)filterButtonTapped:(id)sender
+{
+    //2. Filter
+    NSArray *filteredArray = [[self.sequence filter:^BOOL(id value) {
+        return ([value integerValue] % 2 == 0);
+    }] array];
+    self.answerLabel.text = [self buildTextFromNumberArray:filteredArray];
+}
+
+#pragma mark - Private
+
+- (NSString *)buildTextFromNumberArray:(NSArray *)array
+{
+    //3. Fold
+    return [[array rac_sequence] foldLeftWithStart:@"(" reduce:^id(id accumulator, id value) {
+        if ([value isEqual:[array lastObject]]) {
+            return [accumulator stringByAppendingFormat:@"@(%@))", value];
+        } else {
+            return [accumulator stringByAppendingFormat:@"@(%@), ", value];
+        }
+    }];
 }
 
 @end
