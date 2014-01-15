a576fc84cdfa970ec425e686cfdc10be8069c85c
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2014-01-15 21:43:23 +0900

    Created RAC-Sampler project.

---
+++ RACSampler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RACSampler
+//
+//  Created by jarinosuke on 1/15/14.
+//  Copyright (c) 2014 jarinosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
